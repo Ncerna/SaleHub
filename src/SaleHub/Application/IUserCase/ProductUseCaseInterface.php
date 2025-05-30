57f0a496eb1b95685b533b36838c8cc1aeee0e22
FIIS <nimerce1234@gmail.com>
2025-05-29 19:29:51 -0500

    fix

---
+++ src/SaleHub/Application/IUserCase/ProductUseCaseInterface.php
@@ -0,0 +1,9 @@
+<?php
+interface ProductUseCaseInterface
+{
+    public function create(array $data): void;
+    public function update(int $id, array $data): void;
+    public function delete(int $id): void;
+    public function get(int $id): array;
+    public function list(): array;
+}
